5e75702f64845433418852c48ee1c8b4b2a3167b
Binel Biju <binelbpe@gmail.com>
2024-07-22 15:58:27 +0530

    react 2nd episode

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
-// const heading=React.createElement("h1",{},"hello namaste React! by ")
-// const root=ReactDOM.createRoot(document.getElementById("root"))
-// root.render(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-/* 
-
-<div id="parent">
-
-    <div id="child">
-
-        <h1>I'm H1 Tag</h1>
-
-    </div>
-
-</div> 
-
-
-<div id="parent">
-
-    <div id="child1">
-
-        <h1>I'm H1 Tag-1</h1>
-        <h1>I'm H1 Tag-2</h1>
-
-    </div>
-
-    <div id="child2">
-
-        <h1>I'm H1 Tag-3</h1>
-        <h1>I'm H1 Tag-4</h1>
-
-    </div>
-
-</div> 
-
-*/
-
-// this is the structure for the belwow code
-
-
-const parent = React.createElement("div",{id: "parent"}, [React.createElement("div",{id:"child1"},[React.createElement("h1",{},"This is an H1 tag-1!!"),React.createElement("h1",{},"This is an H1 tag-2!!")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"This is an H1 tag-3!!"),React.createElement("h1",{},"This is an H1 tag-4!!")])])
-
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste Reac ");
+const jsxheading = <h1 id="heading"> Namaste react using JSX</h1>;
 //nested structure by arrays.
 
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
-console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
+console.log(jsxheading);
+ 
